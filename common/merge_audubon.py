1e0f996254cbdcdf617eab90cce2ee16c4ad7b3a
John Hurley <jhurley@mac.com>
2021-01-28 08:58:32 -0800

    some refactoring

--- common/merge_audubon.py
+++ common/merge_audubon.py
@@ -3,22 +3,27 @@ Get count results as Excel from
 https://netapp.audubon.org/CBCObservation/CurrentYear/ResultsByCount.aspx
 """
 
+import io
+import os
+import sys
 from numbers import Number
 from pathlib import Path
+from typing import Optional
 from typing import Tuple
 
 import pandas as pd
-import io
-import sys
 import xlrd
-import os
 
+# from parameters import prepare_parameters_from_file
+from common_paths import local_parameters_path
 from common_paths import raw_data_path, inputs_merge_path
+from dataframe_to_checklist import dataframe_to_checklist
 from datetime_manipulation import normalize_date_for_details
 from local_translation_context import LocalTranslationContext
+from parameters import Parameters
 from service_merge import merge_checklists
 from taxonomy import Taxonomy
-from text_transform import clean_common_names
+from utilities_cbc import read_excel_or_csv_path
 from utilities_excel import excel_columns
 from write_final_checklist import write_final_checklist_spreadsheet
 
@@ -29,9 +34,14 @@ def merge_audubon_results(taxonomy: Taxonomy,
     stem_to_colnames = {}
     parameters = {}
 
-    for fpath in results_path.glob('*.xls'):
-        df, ci = extract_from_audubon_results(fpath)
-        # print(circle_name, circle_code, count_date)
+    sector_files = []
+    for fpath in results_path.glob('*'):
+        if fpath.stem.startswith('~$') or fpath.stem.startswith('.') or fpath.is_dir():
+            continue
+        print(fpath)
+        df = read_excel_or_csv_path(fpath)
+        ci = extract_circle_info(fpath, df)
+
         print(f'Name: {ci["Name"]}, Code: {ci["Code"]}, Date: {ci["Date"]}, Count: {df.shape[0]}')
 
         xparameters = {
@@ -44,22 +54,23 @@ def merge_audubon_results(taxonomy: Taxonomy,
         # ToDo: Service-Merge should not depend on loading parameters #36
         parameters = xparameters
 
-        cleaned_common_names = clean_common_names(df.CommonName, taxonomy,
-                                                  local_translation_context)
-        df.CommonName = cleaned_common_names
-
-        year = ci["Date"][-4:]
+        # cleaned_common_names = clean_common_names(df.CommonName, taxonomy,
+        #                                           local_translation_context)
+        # df.CommonName = cleaned_common_names
+        dfcl = dataframe_to_checklist(df, taxonomy, local_translation_context)
+        print(f'dfcl: {dfcl.shape}')
+        year = ci["Date"][0:4]
         fname = f'{ci["Code"]}-{year}-AudubonResults.xlsx'
         outpath = inputs_merge_path / fname
         col_name = f'{ci["Name"]} ({ci["Code"]}) {xparameters["CountDate"]}'
         stem_to_colnames[outpath.stem] = col_name
+        sector_files.append(outpath)
 
-        write_final_checklist_spreadsheet(df, outpath, xparameters, None)
+        write_final_checklist_spreadsheet(dfcl, outpath, xparameters, None)
 
     # Pick something local as base
     from common_paths import outputs_path
     summary_base = outputs_path / 'CASJ-2020-Single.xlsx'
-    sector_files = list(inputs_merge_path.glob('*.xlsx'))
     summary, cols_to_hide, cols_to_highlight = merge_checklists(summary_base, sector_files,
                                                                 stem_to_colnames, taxonomy,
                                                                 local_translation_context)
@@ -118,6 +129,7 @@ def extract_from_audubon_results(fpath: Path) -> Tuple[pd.DataFrame, dict]:
 
     return df, circle_info
 
+
 # https://stackoverflow.com/questions/7619319/python-xlrd-suppress-warning-messages
 
 # Mute xlrd warnings for OLE inconsistencies
@@ -125,6 +137,7 @@ class LogFilter(io.TextIOWrapper):
     def __init__(self, buffer=sys.stdout, *args, **kwargs):
         # self.buffer = buffer
         super(LogFilter, self).__init__(buffer, *args, **kwargs)
+
     def write(self, data):
         pass
         # if isinstance(data, str):
@@ -136,10 +149,43 @@ class LogFilter(io.TextIOWrapper):
         # else:
         #     super(LogFilter, self).write(data)
 
+
 def open_workbook(file_contents, encoding_override):
     fnull = open(os.devnull, 'w')
-    logfilter = fnull #io.BufferedIOBase #LogFilter()
+    logfilter = fnull  # io.BufferedIOBase #LogFilter()
 
     return xlrd.open_workbook(file_contents=file_contents, logfile=logfilter,
                               encoding_override=encoding_override)
 
+
+def extract_circle_info_from_audubon_dataframe(df: pd.DataFrame) -> dict:
+    circle_info_names = ['Count Name:']
+    circle_info_row = df[df.isin(circle_info_names).any(axis=1)].iloc[0]
+    ci = circle_info_row[circle_info_row != ''].reset_index(drop=True)
+    xdate = normalize_date_for_details(ci[5])
+    circle_info = {'Name': ci[1], 'Code': ci[3], 'Date': xdate}
+
+    return circle_info
+
+
+def extract_circle_info(fpath: Path, df: pd.DataFrame) -> Optional[dict]:
+    if 'CurrentYearResultsByCount' in fpath.stem:
+        return extract_circle_info_from_audubon_dataframe(df)
+
+    # This assumes file has circle code as first 4 characters, e.g.
+    #     CAPA-pacbc_totals_2020.xlsx for 'CAPA'
+    name_prefix = fpath.stem[0:9]
+    ppath = local_parameters_path / f'{name_prefix}-Parameters.xlsx'
+    #     print(ppath)
+    if not ppath.exists():
+        return {'Name': fpath.stem, 'Code': 'XXXX', 'Date': '2020-12-XX'}
+
+    parameters_df = read_excel_or_csv_path(ppath, xheader=None)
+    parameters_df = Parameters().prepare_parameters_from_file(parameters_df)
+
+    #     print(parameters_df)
+    return {
+        'Name': parameters_df['CircleName'],
+        'Code': parameters_df['CircleAbbrev'],
+        'Date': parameters_df['CountDate'].strftime("%Y-%m-%d")
+    }
